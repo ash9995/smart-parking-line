3c0252edce9ba0b25a4f517c9ca23f4ac05dd611
Aisha Alshammari <aisharsh@hotmail.com>
2025-12-12 12:46:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // REPLACE 'repo-name' WITH THE EXACT NAME OF YOUR GITHUB REPOSITORY
-  // Example: if your repo is 'parking-system', use '/parking-system/'
-  base: "/YOUR_REPO_NAME/", 
+ 
+  base: "urkn", 
   resolve: {
     alias: {
       "@": path.resolve(__kqdirname, "./src"),
