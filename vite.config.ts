271b553e804e86c769644d162f824273e4dbfd18
Aisha Alshammari <aisharsh@hotmail.com>
2025-12-12 13:10:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
-  base: "urkn", 
+  
+
   resolve: {
     alias: {
-      "@": path.resolve(__kqdirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
